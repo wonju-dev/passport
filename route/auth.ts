47b8772f25803f9e00e71c89ef191effc51f4b67
wonju-dev <wonjulee.dev@gmail.com>
2021-12-17 16:22:49 +0900

    [Feat]Local 로그인 라우터 구성

--- route/auth.ts
+++ route/auth.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import passport from 'passport';
 
 const router = express.Router();
 
-router.post('/local', (req, res) => {
-  console.log('local');
+router.post('/local', passport.authenticate('local', { session: false }), (req, res) => {
+  res.json(req.user);
 });
 
 router.get('/github', (req, res) => {
