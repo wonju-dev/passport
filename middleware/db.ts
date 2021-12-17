0ddf039ae30cf5db17ecb93110a982c2d57bad2c
wonju-dev <wonjulee.dev@gmail.com>
2021-12-17 16:22:00 +0900

    [Feat]Dummy Data 생성

---
+++ middleware/db.ts
@@ -0,0 +1,27 @@
+const userDB = [
+  {
+    username: 'a',
+    password: '1',
+  },
+  {
+    username: 'b',
+    password: '2',
+  },
+  {
+    username: 'c',
+    password: '3',
+  },
+  {
+    username: 'd',
+    password: '4',
+  },
+];
+
+const getUser = (username: string) => userDB.find((user) => user.username === username);
+
+interface Iuser {
+  username: string;
+  password: string;
+}
+
+export { getUser, Iuser };
