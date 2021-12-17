c9f447a9dd850613a25995de12fbb67305840aae
wonju-dev <wonjulee.dev@gmail.com>
2021-12-17 16:20:27 +0900

    [Config] passport 초기화, bodyparser 등록

--- index.ts
+++ index.ts
@@ -2,12 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 
+import passport from 'passport';
+import initStrategies from './middleware';
+
 import { authRouter, indexRouter } from './route';
 
 const app = express();
 dotenv.config();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.use(passport.initialize());
+initStrategies();
+
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
 
